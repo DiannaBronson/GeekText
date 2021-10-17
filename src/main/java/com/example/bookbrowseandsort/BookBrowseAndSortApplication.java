cf44fee5e155703a18185fd00ab1c19e2a6beca2
Dianna Bronson <diannaltbronson@gmail.com>
2021-10-17 12:16:52 -0400

    Branch creation for BookBrowseAndSort

---
+++ src/main/java/com/example/bookbrowseandsort/BookBrowseAndSortApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookbrowseandsort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBrowseAndSortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookBrowseAndSortApplication.class, args);
+    }
+
+}
